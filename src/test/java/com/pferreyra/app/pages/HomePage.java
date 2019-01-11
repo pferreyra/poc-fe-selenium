c626a6a3d64aa2b11bd0c497eba38eb52c26bd5a
Paula Mariel Ferreyra <paulaferreyra@Paulas-MacBook-Pro.local>
2019-01-10 23:05:56 -0300

    WIP POM POC FE Selenium

---
+++ src/test/java/com/pferreyra/app/pages/HomePage.java
@@ -0,0 +1,14 @@
+package com.pferreyra.app.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+  WebDriver driver;
+
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
