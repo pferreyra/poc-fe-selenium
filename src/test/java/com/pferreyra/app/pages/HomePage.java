86e8c9d341a63c90074e11192cac2b0904a55cc8
Paula Ferreyra <paula.ferreyra@synapse.com>
2019-01-14 19:08:03 -0300

    HomePage PO WIP

--- src/test/java/com/pferreyra/app/pages/HomePage.java
+++ src/test/java/com/pferreyra/app/pages/HomePage.java
@@ -1,9 +1,16 @@
 package com.pferreyra.app.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
   WebDriver driver;
+  By origen = By.xpath("//input[class*='sbox-origin']");
+  By destino = By.xpath("//input[class*='sbox-destination");
+  By fechaIda = By.xpath("//input[class*='sbox-checkin-date");
+  By fechaVuelta = By.xpath("//input[class*='sbox-checkout-date");
+  
+  
 
 
   public static void main(String[] args) {
