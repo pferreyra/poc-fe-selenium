04fdc4d782539eea775bd2ea90cf7f6261a836ad
Paula Mariel Ferreyra <paulaferreyra@Paulas-MacBook-Pro.local>
2019-01-14 23:25:43 -0300

    WIP PO HomePage

--- src/test/java/com/pferreyra/app/pages/HomePage.java
+++ src/test/java/com/pferreyra/app/pages/HomePage.java
@@ -5,12 +5,13 @@ import org.openqa.selenium.WebDriver;
 
 public class HomePage {
   WebDriver driver;
-  By origen = By.xpath("//input[class*='sbox-origin']");
-  By destino = By.xpath("//input[class*='sbox-destination");
-  By fechaIda = By.xpath("//input[class*='sbox-checkin-date");
-  By fechaVuelta = By.xpath("//input[class*='sbox-checkout-date");
-  
-  
+  By origen = By.xpath("input[class*='sbox-origin']");
+  By destino = By.xpath("input[class*='sbox-destination");
+  By fechaIda = By.xpath("input[class*=sbox-checkin-date]");
+  By fechaVuelta = By.xpath("input[class*='sbox-checkout-date");
+  By buscar = By.xpath("//div[@class='sbox-button-horizontal-medium']//em[@class='btn-text']");
+
+
 
 
   public static void main(String[] args) {
