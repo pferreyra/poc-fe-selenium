0b1532f8ce9116d2bf5dc28bc2e002193389e246
Paula Ferreyra <paula.ferreyra@synapse.com>
2019-01-21 19:30:11 -0300

    WIP PO HomePage funcion ingresarFechaDesde

--- src/test/java/com/pferreyra/app/pages/HomePage.java
+++ src/test/java/com/pferreyra/app/pages/HomePage.java
@@ -2,6 +2,10 @@ package com.pferreyra.app.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 
 public class HomePage {
   WebDriver driver;
@@ -11,6 +15,8 @@ public class HomePage {
   By fechaIda = By.xpath("input[class*=sbox-checkin-date]");
   By fechaVuelta = By.xpath("input[class*='sbox-checkout-date");
   By buscar = By.xpath("//div[@class='sbox-button-horizontal-medium']//em[@class='btn-text']");
+  By fechas = By.xpath("//input[contains(@type,'text') and contains(@class,'sbox-checkin-date')]");
+  List<WebElement> calendario = driver.findElements(By.xpath("//span[contains(@class,'_dpmg2--date _dpmg2--available')]"));
 
   public HomePage (WebDriver driver){
     this.driver = driver;
@@ -22,11 +28,15 @@ public class HomePage {
   }
 
   // Ingresar Origen
-
   public void ingresarOrigen (String desde) {
-
     driver.findElement(origen).sendKeys(desde);
-
+  }
+  
+  //Ingresar Fecha Desde
+  public void ingresarFechaDesde (List<WebElement> calendario, int i) {
+	  this.calendario = calendario;
+	  driver.findElement(fechas).click();
+	  calendario.get(i).click();
   }
 
 
