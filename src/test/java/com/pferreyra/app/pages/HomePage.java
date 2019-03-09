82a7fe3441fa03b22b7f719c5d3ee7ded52aff08
Paula Mariel Ferreyra <paulaferreyra@Paulas-MacBook-Pro.local>
2019-03-08 23:14:38 -0300

    Refactor de BuscarPaquete a BuscarProducto, cambio a Mercado Libre, agregado de selenium-api al POM, test case para abrir el sitio, ingresar texto en el input de búsqueda

--- src/test/java/com/pferreyra/app/pages/HomePage.java
+++ src/test/java/com/pferreyra/app/pages/HomePage.java
@@ -2,41 +2,31 @@ package com.pferreyra.app.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-import java.util.List;
 
 
 public class HomePage {
-  WebDriver driver;
-  final String url = "https://www.despegar.com.ar/";
-  By origen = By.xpath("input[class*='sbox-origin']");
-  By destino = By.xpath("input[class*='sbox-destination");
-  By fechaIda = By.xpath("input[class*=sbox-checkin-date]");
-  By fechaVuelta = By.xpath("input[class*='sbox-checkout-date");
-  By buscar = By.xpath("//div[@class='sbox-button-horizontal-medium']//em[@class='btn-text']");
-  By fechas = By.xpath("//input[contains(@type,'text') and contains(@class,'sbox-checkin-date')]");
-  List<WebElement> calendario = driver.findElements(By.xpath("//span[contains(@class,'_dpmg2--available')]"));
+  private WebDriver driver;
+  private final String url = "https://www.mercadolibre.com.ar/";
+  private By buscar = By.name("as_word");
+
 
   public HomePage (WebDriver driver){
     this.driver = driver;
   }
 
-  //Ingresar a la página
+  /**
+   * Ingresar a la página
+   */
   public void ingresarSitio () {
     driver.get(url);
   }
 
-  // Ingresar Origen
-  public void ingresarOrigen (String desde) {
-    driver.findElement(origen).sendKeys(desde);
-  }
-  
-  //Ingresar Fecha Desde
-  public void ingresarFechaDesde (List<WebElement> calendario, int i) {
-	  this.calendario = calendario;
-	  driver.findElement(fechas).click();
-	  calendario.get(i).click();
+  /**
+   * Ingresar una búsqueda de producto
+   * @param busqueda texto del producto a buscar
+   */
+  public void ingresarBusqueda (String busqueda) {
+    driver.findElement(buscar).sendKeys(busqueda);
   }
 
 
--- src/test/java/com/pferreyra/app/tests/BuscarProducto.java
+++ src/test/java/com/pferreyra/app/tests/BuscarProducto.java
@@ -1,7 +1,5 @@
 package com.pferreyra.app.tests;
 
-import com.pferreyra.app.pages.HomePage;
-import static org.junit.jupiter.api.Assertions.fail;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.AfterEach;
@@ -10,15 +8,18 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.pferreyra.app.pages.HomePage;
 
 
-class BuscarPaquete {
+
+class BuscarProducto {
 
   WebDriver driver;
-  HomePage busquedaPaquete;
+  HomePage busquedaProducto;
 
   @BeforeEach
   void setUp() throws Exception {
+    System.setProperty("webdriver.gecko.driver", "//Users//paulaferreyra//Java//Repositorios//Selenium//geckodriver");
     driver = new FirefoxDriver();
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     driver.manage().window().maximize();
@@ -29,11 +30,12 @@ class BuscarPaquete {
   }
 
   @Test
-  public void test() {
-	  busquedaPaquete = new HomePage(driver);
-	  busquedaPaquete.ingresarSitio();
-	  String desde = "Buenos Aires";
-	  busquedaPaquete.ingresarOrigen(desde);
+  public void test() throws InterruptedException {
+    busquedaProducto = new HomePage(driver);
+    busquedaProducto.ingresarSitio();
+    Thread.sleep(20000);
+    String busqueda = "Notebook";
+    busquedaProducto.ingresarBusqueda(busqueda);
   }
 
 }
