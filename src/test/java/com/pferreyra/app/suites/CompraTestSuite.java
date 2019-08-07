1d916af789cc791243965fd69db3e7d70e35e927
Paula Ferreyra <paula.ferreyra@synapse.com>
2019-08-07 18:35:46 -0300

    WIP test Ingresar al checkout

--- src/test/java/com/pferreyra/app/suites/CompraTestSuite.java
+++ src/test/java/com/pferreyra/app/suites/CompraTestSuite.java
@@ -1,18 +1,12 @@
 package com.pferreyra.app.suites;
 
 import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 import com.pferreyra.app.tests.*;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectClasses;
 
 @RunWith(JUnitPlatform.class)
-@SelectClasses ({
-	BuscarProducto.class,
-	FiltrarProductos.class,
-	SeleccionarUnProducto.class
-	
-})
+@SelectClasses(BuscarProducto.class)
 public class CompraTestSuite {
 
 }
---
+++ src/test/java/com/pferreyra/app/tests/IngresarAlCheckout.java
@@ -0,0 +1,48 @@
+package com.pferreyra.app.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.pferreyra.app.pages.HomePage;
+import com.pferreyra.app.pages.ListadoResultados;
+import com.pferreyra.app.pages.VistaProducto;
+
+class IngresarAlCheckout {
+	  WebDriver driver;
+	  ListadoResultados filtradoProducto;
+	  HomePage busquedaProducto;
+	  VistaProducto detalleProducto;
+
+	@BeforeEach
+	  void setUp() throws Exception {
+		System.setProperty("webdriver.gecko.driver",
+				"C:\\Users\\paula.ferreyra\\eclipse-workspace\\selenium\\geckodriver.exe");
+		driver = new FirefoxDriver();
+	  }
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIngresarAlCheckout() {
+		 busquedaProducto = new HomePage(driver);
+		    String busqueda = "notebook";
+		    busquedaProducto.ingresarBusqueda(busqueda);
+		    filtradoProducto = new ListadoResultados(driver);
+			String tituloProducto = filtradoProducto.seleccionarPrimerItem();
+			if (tituloProducto == null) {
+				System.out.println("No hay productos listados");
+			}
+			else {
+			detalleProducto = new VistaProducto(driver);
+			}
+	}
+
+}
